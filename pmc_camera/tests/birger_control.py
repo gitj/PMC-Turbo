601f9a691ae37616baf4d2cf124d3f7a197b5be9
gitj <glenn.caltech@gmail.com>
2016-08-03 11:22:06 -0400

    add unpacking routine add timeout to make birger_control work

--- pmc_camera/tests/birger_control.py
+++ pmc_camera/tests/birger_control.py
@@ -7,19 +7,20 @@ class birger():
         self.s = serial.Serial()
         self.s.port = port
         self.s.baudrate = 115200
-        self.s.timeout = 0.01
+        self.s.timeout = 0.5
         self.s.bytesize = 8
         self.stopbits = 1
 
     def sendget(self, msg, wait=2):
         self.s.open()	
         self.s.write(msg)
-        #tic = time.time()
-        resp = ''
-        while self.s.inWaiting()>0:
-             resp += self.s.read(1)
-        #while time.time()-tic < wait:
-        #    resp += self.s.read()
+        tic = time.time()
+        resp = self.s.read()
+#        while self.s.inWaiting()>0:
+#             resp += self.s.read(1)
+        while resp and (time.time()-tic < wait):
+            last = self.s.read()
+            resp += last
         #    time.sleep(0.001)
         # Tried using a timeout - also didn't work
         print resp
