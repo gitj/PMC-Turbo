9646b6ba72d6c1965b9a6b79b97964e1e778ce30
gitj <glenn.caltech@gmail.com>
2016-08-04 15:36:27 -0400

    basic auto exposure setting seems to work

--- pmc_camera/pycamera/pycamera.py
+++ pmc_camera/pycamera/pycamera.py
@@ -6,6 +6,7 @@ class PyCamera():
     def __init__(self,ip="10.0.0.2",num_buffers=16):
         self._pc = _PyCamera(ip=ip,num_buffers=num_buffers)
         self._num_buffers = num_buffers
+        self.exposure_counts = 0
     def get_image(self, unpack=True, dimensions=(3232,4864)):
         """
         get the image data from the next waiting buffer
@@ -47,6 +48,63 @@ class PyCamera():
         result = self._pc.set_parameter_from_string("FreerunExposureTime", str(counts))
         if result != 0:
             raise Exception("Camera returned error code %d when trying to set exposure value to %d." % (result,counts))
+        self.exposure_counts = counts
 
-    def auto_adjust_exposure(self):
+    def simple_exposure_adjust(self,goal=4000,percentile=99.9,step_factor=1.5,downsample=16,
+                               start_fresh=False,verbose=False):
+        def debug(msg):
+            if verbose:
+                print msg
+
+        if start_fresh or self.exposure_counts == 0:
+            exposure = 1
+            last_exposure = 1
+        else:
+            exposure = self.exposure_counts
+            for k in range(self._num_buffers):
+                self.get_image(unpack=False)
+            debug("flushed buffers")
+            d = self.get_image()[1:-1:downsample,1:-1:downsample]
+            score = np.percentile(d.flat,percentile)/16.
+            if score > goal:
+                step_factor = 1/step_factor
+            last_exposure = self.exposure_counts
+
+
+        done = False
+        while not done:
+            self.set_exposure_counts(exposure)
+            debug("Trying %d" % exposure)
+            for k in range(self._num_buffers):
+                self.get_image(unpack=False)
+            debug("flushed buffers")
+            d = self.get_image()[1:-1:downsample,1:-1:downsample]
+            score = np.percentile(d.flat,percentile)/16.
+            debug("current score: %.1f" % score)
+            if step_factor > 1 and score > goal:
+                debug("goal exceeded, finishing")
+                done = True
+            elif step_factor < 1 and score < goal:
+                debug("goal reached, finishing")
+                last_exposure = exposure
+                done = True
+            else:
+                last_exposure = exposure
+                exposure = int(np.round(exposure*step_factor))
+                if exposure == last_exposure:
+                    if step_factor > 1:
+                        exposure = exposure + 1
+                    else:
+                        exposure = exposure - 1
+                if exposure > 65535:
+                    debug("reached max exposure, finishing")
+                    done = True
+                if exposure == 1:
+                    debug("reached min exposure, finishing")
+                    done = True
+        self.set_exposure_counts(last_exposure)
+        return last_exposure
+
+
+
         pass
