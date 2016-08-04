d805036f900f4b45ed66be4b34481f5ecf0d6f39
pmc-user <pmc@pmc-camera-0.unassigned-domain>
2016-08-04 16:47:47 -0400

    Wrapped birger commands in python, added status tracking. Needs a lot of error checking.

--- pmc_camera/tests/birger_control.py
+++ pmc_camera/tests/birger_control.py
@@ -5,6 +5,7 @@ class birger():
 
     def __init__(self, port='/dev/ttyUSB0'):
         self.initialize(port)
+        return
 
     def initialize(self, port):
         self.s = serial.Serial()
@@ -13,9 +14,14 @@ class birger():
         self.s.timeout = 0.5
         self.s.bytesize = 8
         self.stopbits = 1
-        self.state_dict = {'focus', 'aperature'}
-        self.flush_buffer()
-        self.set_protocol
+        self.state_dict = {'focus', 'aperture'}
+        check = self.flush_buffer()
+        check = self.set_protocol
+        self.initialize_aperture()
+        self.ap_min, self.ap_max = (0, self.find_aperture_range())
+        self.ap_pos = int(self.ap_max)
+        # When we find the ap_range we set the ap_pos to max (fully closed)
+        self.fmin, self.fmax, self.f_pos = self.find_focus_and_range() 
 
     def sendget(self, msg, wait=0.5, terminator='\r'):
         self.s.open()	
@@ -50,11 +56,44 @@ class birger():
             self.set_protocol()
         return
 
-    def set_aperature(self, aperature):
+    def initialize_aperture(self):
+        # Required when the aperature hasn't been initialized.
+        return self.sendget('in')
+
+    def find_aperture_range(self):
+        # Closes aperture fully, gets info to find what absolute step it is at.
+        # Slices string and returns max.
+        response = self.sendget('mc')
+        return int(self.sendget('pa').split(',')[0])
+
+    def move_aperture(self, steps):
+        # Moves aperture incremental steps.
+        # Negative for open, positive for close.
+        # This should return something for reaching limit.
+        response = self.sendget('mn' + str(steps))
+        steps_taken = int(response.split(',')[0].strip('DONE'))
+        self.ap_pos += steps_taken
         return
 
-    def set_focus(self, focus):
+    def find_focus_and_range(self):
+        #self.s.timeout = 3
+        # la takes some time
+        #response = self.sendget('la0')
+        #self.s.timeout = 0.5
+        focus_range_string = self.sendget('fp')
+        raw = focus_range_string.split(':')
+        return (int(raw[1].strip('fmax')), int(raw[2].strip('current')), int(raw[3].strip('\r')))
+        # min, max, current
+        # We should discuss how often to redo the la command.
+        # Seems like we shouldn't do it every time, however.
+
+    def move_focus(self, steps):
+        response = self.sendget('mf'+str(steps))
+        steps_taken = int(response.split(',')[0].strip('DONE'))
+        self.f_pos += steps_taken
         return
 
-    def learn_focus(self, focus):
-        return
+    def print_status(self):
+        print 'Ap min: %d, Ap max: %d, Current ap: %d' % (self.ap_min, self.ap_max, self.ap_pos)
+        print 'f min: %d, f max: %d, Current f: %d' % (self.fmin, self.fmax, self.f_pos)
+    
