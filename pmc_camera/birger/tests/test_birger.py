b64c66b1fb1a33926ba340c488cd62b1b6378075
pmc-user <pmc@pmc-camera-0.unassigned-domain>
2016-08-22 11:19:03 -0400

    Adding birger testing.

---
+++ pmc_camera/birger/tests/test_birger.py
@@ -0,0 +1,12 @@
+import birger
+
+def wrong_protocol():
+    # sets wrong protocol, makes sure birger handles it appropriately.
+    return
+
+def lost_connection():
+    return
+
+def un_initialize():\
+    # Birger isn't initialized - make sure birger handles it.
+    return
