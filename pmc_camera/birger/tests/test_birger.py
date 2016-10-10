7e51b801fcb8fe3972d4d159f63f62f82ef43131
gitj <glenn.caltech@gmail.com>
2016-10-10 16:23:50 -0400

    update unused file

--- pmc_camera/birger/tests/test_birger.py
+++ pmc_camera/birger/tests/test_birger.py
@@ -1,4 +1,4 @@
-import birger
+import pmc_camera.birger.birger_control
 
 def wrong_protocol():
     # sets wrong protocol, makes sure birger handles it appropriately.
@@ -7,6 +7,6 @@ def wrong_protocol():
 def lost_connection():
     return
 
-def un_initialize():\
+def un_initialize():
     # Birger isn't initialized - make sure birger handles it.
     return
