b6f8e3a7054a47157846f1ee7ad0d9ea96c77a3f
bjorn <bkjell@gmail.com>
2016-11-04 16:57:02 -0400

    Switched default IP address in tests to localhost. Hopefully makes it pass on all computers (including travis-ci)

--- pmc_camera/communication/tests/udp_receiver_tests.py
+++ pmc_camera/communication/tests/udp_receiver_tests.py
@@ -2,7 +2,7 @@ from pmc_camera.communication import generate_bad_packets, udp_receiver
 
 
 def simple_test():
-    u = udp_receiver.UDPReceiver()
+    u = udp_receiver.UDPReceiver(ip='localhost', port=4001)
     u.put_bytes_in_buffer('\x10\x13\x03')
     u.process_buffer()
     assert (u.buffer == '\x10\x13\x03')
@@ -10,7 +10,7 @@ def simple_test():
 
 
 def junk_before_test():
-    u = udp_receiver.UDPReceiver()
+    u = udp_receiver.UDPReceiver(ip='localhost', port=4001)
     bytes = generate_bad_packets.generate_random_bytes(100) + '\x10\x13\x03'
     u.put_bytes_in_buffer(bytes)
     u.process_buffer()
@@ -19,7 +19,7 @@ def junk_before_test():
 
 
 def junk_after_test():
-    u = udp_receiver.UDPReceiver()
+    u = udp_receiver.UDPReceiver(ip='localhost', port=4001)
     bytes = '\x10\x13\x03' + generate_bad_packets.generate_random_bytes(100)
     u.put_bytes_in_buffer(bytes)
     u.process_buffer()
@@ -28,7 +28,7 @@ def junk_after_test():
 
 
 def two_packets_test():
-    u = udp_receiver.UDPReceiver()
+    u = udp_receiver.UDPReceiver(ip='localhost', port=4001)
     bytes = '\x10\x13\x03' + '\x10\x13\x03'
     u.put_bytes_in_buffer(bytes)
     u.process_buffer()
