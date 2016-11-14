a4ceb1ae496e5da6a61065bac724f5086198151b
bjorn <bkjell@gmail.com>
2016-11-14 12:19:14 -0500

    Changed communicator_tests to use unittest.TestCase class.

--- pmc_camera/communication/tests/communicator_tests.py
+++ pmc_camera/communication/tests/communicator_tests.py
@@ -1,72 +1,53 @@
 import socket
+import unittest
 from pmc_camera.communication import camera_communicator, science_communication, SIP_communication_simulator
 from pmc_camera.pipeline import basic_pipeline
 
 
-def test_initial_master_decision():
-    communicator0 = camera_communicator.Communicator(0)
-    communicator1 = camera_communicator.Communicator(1)
-    communicator0.start_pyro_thread()
-    communicator1.start_pyro_thread()
+class TwoCommunicatorTests(unittest.TestCase):
+    def setUp(self):
+        self.communicator0 = camera_communicator.Communicator(0)
+        self.communicator1 = camera_communicator.Communicator(1)
+        self.communicator0.start_pyro_thread()
+        self.communicator1.start_pyro_thread()
 
-    communicator0.identify_leader()
-    communicator1.identify_leader()
-    assert (communicator0.leader_handle == communicator0.peers[0])
-    assert (communicator1.leader_handle == communicator1.peers[0])
-    assert (communicator0.leader_handle == communicator0.leader_handle)
+    def tearDown(self):
+        self.communicator0.end_loop = True
+        self.communicator1.end_loop = True
+        self.communicator0.__del__()
+        self.communicator1.__del__()
 
-    communicator0.end_loop = True
-    communicator1.end_loop = True
-    communicator0.__del__()
-    communicator1.__del__()
+    def test_initial_master_decision(self):
+        self.communicator0.identify_leader()
+        self.communicator1.identify_leader()
+        self.assertEqual(self.communicator0.leader_handle, self.communicator0.peers[0])
+        self.assertEqual(self.communicator1.leader_handle, self.communicator1.peers[0])
+        self.assertEqual(self.communicator0.leader_handle, self.communicator1.leader_handle)
 
-
-def test_pings():
-    communicator0 = camera_communicator.Communicator(0)
-    communicator1 = camera_communicator.Communicator(1)
-    communicator0.start_pyro_thread()
-    communicator1.start_pyro_thread()
-
-    assert (communicator0.ping_other(communicator0.peers[1]) == True)
-    assert (communicator1.ping_other(communicator0.peers[0]) == True)
-
-    communicator0.end_loop = True
-    communicator1.end_loop = True
-    communicator0.__del__()
-    communicator1.__del__()
+    def test_pings(self):
+        assert (self.communicator0.ping_other(self.communicator0.peers[1]) == True)
+        assert (self.communicator1.ping_other(self.communicator0.peers[0]) == True)
 
 
-def test_setup_sip_port(sip_ip='localhost', sip_port=4001):
-    # sip_ip='192.168.1.137', sip_port=4001
-    communicator0 = camera_communicator.Communicator(0)
+class OneCommunicatorTests(unittest.TestCase):
+    def setUp(self):
+        self.communicator = camera_communicator.Communicator(0)
+        self.communicator.start_pyro_thread()
+        self.communicator.setup_leader_attributes(sip_ip='localhost', sip_port=4001)
 
-    communicator0.setup_leader_attributes(sip_ip, sip_port)
+    def tearDown(self):
+        self.communicator.end_loop = True
+        self.communicator.__del__()
 
-    communicator0.end_loop = True
-    communicator0.__del__()
-
-
-def test_receive_sip_message(sip_ip='localhost', sip_port=4001):
-    sip_simulator_post = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    communicator0 = camera_communicator.Communicator(0)
-    communicator0.setup_leader_attributes(sip_ip, sip_port)
-
-    sip_packet = SIP_communication_simulator.construct_science_request_packet()
-
-    sip_simulator_post.sendto(sip_packet, (sip_ip, sip_port))
-
-    communicator0.get_bytes_from_sip_socket()
-    communicator0.__del__()
-
-    # print sip_packet
-    # print communicator0.sip_packet_decoder.buffer
-    # print sip_packet == communicator0.sip_packet_decoder.buffer
-
-    assert (communicator0.sip_packet_decoder.buffer == sip_packet)
-    # Need to figure out a way to put the bytes I want in here - set up socket
-    # assert(results of command)
+    def test_receive_sip_message(self):
+        sip_simulator_port = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sip_packet = SIP_communication_simulator.construct_science_request_packet()
+        sip_simulator_port.sendto(sip_packet, ('localhost', 4001))
+        self.communicator.get_bytes_from_sip_socket()
+        assert (self.communicator.sip_packet_decoder.buffer == sip_packet)
 
 
+'''
 def process_packet(packet):
     return
     # camera_communicator.Communicator(0)
@@ -88,3 +69,4 @@ def process_command_queue(command):
 def test_image_capture():
     return
     # assert (images as expected)
+'''
