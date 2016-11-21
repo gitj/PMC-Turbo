1f0ee07fcac9c04ea2640ef79d2f42691fc6ddb1
bjorn <bkjell@gmail.com>
2016-11-21 13:11:56 -0500

    Removed len and which from science command - this will be interpreted elsewhere. Adjusted tests to take this into account.

--- pmc_camera/communication/tests/communicator_tests.py
+++ pmc_camera/communication/tests/communicator_tests.py
@@ -1,6 +1,7 @@
 import socket
 import unittest
-from pmc_camera.communication import camera_communicator, science_communication, SIP_communication_simulator
+from pmc_camera.communication import camera_communicator, science_communication, SIP_communication_simulator, \
+    generate_bad_packets
 from pmc_camera.pipeline import basic_pipeline
 
 
@@ -45,19 +46,48 @@ class OneCommunicatorTests(unittest.TestCase):
 
     def test_process_packet(self):
         packet = SIP_communication_simulator.construct_science_command_packet(2, '\x20\x21\x22\x23\x24')
-        expected_packet_dict = {'title': 'science_data_command', 'value': '\x20\x21\x22\x23\x24', 'which': 2}
+        expected_packet_dict = {'title': 'science_data_command', 'value': '\x02\x20\x21\x22\x23\x24'}
         self.communicator.packet_queue.put(packet)
         self.communicator.process_packet_queue()
-        self.assertEqual(self.communicator.command_queue.get(), expected_packet_dict)
+        actual_packet_dict = self.communicator.command_queue.get()
+        #print actual_packet_dict
+        #print expected_packet_dict
+        self.assertEqual(actual_packet_dict, expected_packet_dict)
 
     def test_whole_sip_pipeline(self):
         packet = SIP_communication_simulator.construct_science_command_packet(2, '\x20\x21\x22\x23\x24')
-        expected_packet_dict = {'title': 'science_data_command', 'value': '\x20\x21\x22\x23\x24', 'which': 2}
+        expected_packet_dict = {'title': 'science_data_command', 'value': '\x02\x20\x21\x22\x23\x24'}
         sip_simulator_port = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sip_simulator_port.sendto(packet, ('localhost', 4001))
         self.communicator.process_sip_socket()
         self.communicator.process_packet_queue()
         self.assertEqual(self.communicator.command_queue.get(), expected_packet_dict)
+
+    def test_junk_packet(self):
+        packet = generate_bad_packets.generate_random_bytes(length=100)
+        sip_simulator_port = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sip_simulator_port.sendto(packet, ('localhost', 4001))
+        self.communicator.process_sip_socket()
+        self.communicator.process_packet_queue()
+        self.assertEqual(self.communicator.packet_queue.empty(), True)
+
+    def test_end_byte_in_center(self):
+        packet = SIP_communication_simulator.construct_science_command_packet(2, '\x00\x01\x02\x03\x04\x05')
+        expected_packet_dict_list = [{'title': 'science_data_command', 'value': '\x02\x00\x01\x02\x03\x04\x05'},
+                                     {'title': 'science_data_command', 'value': '\x02\x00\x01\x02'}]
+        sip_simulator_port = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sip_simulator_port.sendto(packet, ('localhost', 4001))
+        self.communicator.process_sip_socket()
+        self.communicator.process_packet_queue()
+        packet_dict_list = []
+        while not self.communicator.command_queue.empty():
+            packet_dict_list.append(self.communicator.command_queue.get())
+        for packet in expected_packet_dict_list:
+            assert (packet in packet_dict_list)
+
+            # To do: add packets that are bad and make sure the communicator behaves well.
+            # In particular, add packets with multiple end_bytes in them and make sure the good packets get processed
+            # Same with start_bytes.
 
 
 '''
