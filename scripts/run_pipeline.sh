6e5f16f23c98f62347f6bb04dd66e5d18cfb59e0
gitj <glenn.caltech@gmail.com>
2016-11-27 23:20:27 -0500

    another path

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -2,5 +2,6 @@
 export PATH=/home/pmc/miniconda2/bin:$PATH
 source activate pmc
 export PYTHONPATH=/home/pmc/pmc-turbo
+export GENICAM_GENTL64_PATH=$GENICAM_GENTL64_PATH:"/home/pmc/Vimba_2_0/VimbaGigETL/CTI/x86_64bit"
 export LD_LIBRARY_PATH=/home/pmc/Vimba_2_0/VimbaCPP/DynamicLib/x86_64bit
 python /home/pmc/pmc-turbo/pmc_camera/utils/run_pipeline.py
