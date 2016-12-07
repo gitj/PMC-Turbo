fe216bd59bb78370278500b7688197a376e581ae
bjorn <bkjell@gmail.com>
2016-12-07 14:50:23 -0500

    - Moved hirate_receiving and sending methods to dedicated files, revamped development programs as examples. - Moving constants into constants.py - cobs_tests now uses cobs_encoding package - Wrote simple hirate tests

---
+++ pmc_camera/communication/hirate/hirate_sending_methods.py
@@ -0,0 +1,21 @@
+from __future__ import division
+import struct
+import socket
+from pmc_camera.communication import constants
+import numpy as np
+
+def send(msg, ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(msg, (ip, port))
+    sock.close()
+
+def chunk_data_by_size(chunk_size, file_id, data):
+    chunks = []
+    num_chunks = np.ceil(len(data) / chunk_size)
+    for i in range(int(num_chunks)):
+        msg = data[(i * chunk_size):((i + 1) * chunk_size)]
+        format_string = '4B%ds' % len(msg)
+        chunk = struct.pack(format_string, constants.START_BYTE, file_id, i, num_chunks, msg)
+        # chunk += struct.pack('1B', END_BYTE)
+        chunks.append(chunk)
+    return chunks
--- pmc_camera/communication/tests/cobs_tests.py
+++ pmc_camera/communication/tests/cobs_tests.py
@@ -1,21 +1,25 @@
-import cobs.cobs
 import unittest
 import numpy as np
 import struct
+from pmc_camera.communication.hirate import cobs_encoding
 
-def encode_data(data):
+
+'''
+def encode_data_old(data):
     a = cobs.cobs.encode(data)
     b = a.replace('\x10', '\x00')
     c = cobs.cobs.encode(b)
     d = c.replace('\x10', '\x00')
     return d
 
-def decode_data(data):
+def decode_data_old(data):
     e = data.replace('\x00', '\x10')
     f = cobs.cobs.decode(e)
     g = f.replace('\x00', '\x10')
     h = cobs.cobs.decode(g)
     return h
+'''
+
 
 class CobsTests(unittest.TestCase):
     def setUp(self):
@@ -27,49 +31,49 @@ class CobsTests(unittest.TestCase):
     def test_null_case(self):
         buffer = '\x10\x14\x06\x11\x11\x11\x11\x11\x11\x03'
         data = buffer[1:]
-        encoded_data = encode_data(data)
-        assert(encoded_data.find('\x10') == -1)
-        decoded_data = decode_data(encoded_data)
-        assert(decoded_data == data)
+        encoded_data = cobs_encoding.encode_data(data)
+        assert (encoded_data.find('\x10') == -1)
+        decoded_data = cobs_encoding.decode_data(encoded_data)
+        assert (decoded_data == data)
 
     def test_all_zeros(self):
         buffer = '\x10\x14\x06\x00\x00\x00\x00\x00\x00\x03'
         data = buffer[1:]
-        encoded_data = encode_data(data)
-        assert(encoded_data.find('\x10') == -1)
-        decoded_data = decode_data(encoded_data)
-        assert(decoded_data == data)
+        encoded_data = cobs_encoding.encode_data(data)
+        assert (encoded_data.find('\x10') == -1)
+        decoded_data = cobs_encoding.decode_data(encoded_data)
+        assert (decoded_data == data)
 
     def test_one_10(self):
-        buffer =  '\x10\x14\x06\x11\x11\x10\x11\x11\x11\x03'
+        buffer = '\x10\x14\x06\x11\x11\x10\x11\x11\x11\x03'
         data = buffer[1:]
-        encoded_data = encode_data(data)
-        assert(encoded_data.find('\x10') == -1)
-        decoded_data = decode_data(encoded_data)
-        assert(decoded_data == data)
+        encoded_data = cobs_encoding.encode_data(data)
+        assert (encoded_data.find('\x10') == -1)
+        decoded_data = cobs_encoding.decode_data(encoded_data)
+        assert (decoded_data == data)
 
     def test_all_10(self):
-        buffer =  '\x10\x14\x06\x10\x10\x10\x10\x10\x10\x03'
+        buffer = '\x10\x14\x06\x10\x10\x10\x10\x10\x10\x03'
         data = buffer[1:]
-        encoded_data = encode_data(data)
-        assert(encoded_data.find('\x10') == -1)
-        decoded_data = decode_data(encoded_data)
-        assert(decoded_data == data)
+        encoded_data = cobs_encoding.encode_data(data)
+        assert (encoded_data.find('\x10') == -1)
+        decoded_data = cobs_encoding.decode_data(encoded_data)
+        assert (decoded_data == data)
 
     def test_alternating_0_and_10(self):
-        buffer =  '\x10\x14\x06\x00\x10\x00\x10\x00\x10\x03'
+        buffer = '\x10\x14\x06\x00\x10\x00\x10\x00\x10\x03'
         data = buffer[1:]
-        encoded_data = encode_data(data)
-        assert(encoded_data.find('\x10') == -1)
-        decoded_data = decode_data(encoded_data)
-        assert(decoded_data == data)
+        encoded_data = cobs_encoding.encode_data(data)
+        assert (encoded_data.find('\x10') == -1)
+        decoded_data = cobs_encoding.decode_data(encoded_data)
+        assert (decoded_data == data)
 
     def test_random_data(self):
         np.random.seed(0)
         random_msg = np.random.randint(0, 255, size=10000).astype('uint8')
         format_string = '%dB' % len(random_msg)
         packet = struct.pack(format_string, *random_msg)
-        encoded_data = encode_data(packet)
-        assert(encoded_data.find('\x10') == -1)
-        decoded_data = decode_data(encoded_data)
-        assert(decoded_data == packet)
+        encoded_data = cobs_encoding.encode_data(packet)
+        assert (encoded_data.find('\x10') == -1)
+        decoded_data = cobs_encoding.decode_data(encoded_data)
+        assert (decoded_data == packet)
