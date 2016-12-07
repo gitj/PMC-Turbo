fecd7d383e2eb8ec93407caee0798cb1fa31a1a7
gitj <glenn.caltech@gmail.com>
2016-12-06 20:32:25 -0500

    make pipeline killable by supervisord

--- pmc_camera/utils/run_pipeline.py
+++ pmc_camera/utils/run_pipeline.py
@@ -2,10 +2,12 @@ import logging
 from pmc_camera.pipeline.basic_pipeline import BasicPipeline
 import pmc_camera.utils.log
 import time
+import signal
 
 
 if __name__ == "__main__":
     pmc_camera.utils.log.setup_stream_handler()
     pmc_camera.utils.log.setup_file_handler('pipeline')
     bpl = BasicPipeline(disks_to_use=['/data1','/data2','/data3','/data4'],default_write_enable=1)
+    signal.signal(signal.SIGTERM,bpl.exit)
     bpl.run_pyro_loop()
