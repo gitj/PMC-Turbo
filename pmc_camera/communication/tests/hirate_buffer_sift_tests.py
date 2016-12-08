c1dbb1b30924533670cefc54d0cc09002c606692
bjorn <bkjell@gmail.com>
2016-12-08 16:33:55 -0500

    Added extra tests for full coverage of sip sifter function.

--- pmc_camera/communication/tests/hirate_buffer_sift_tests.py
+++ pmc_camera/communication/tests/hirate_buffer_sift_tests.py
@@ -23,17 +23,31 @@ class HirateBufferSiftTests(unittest.TestCase):
         np.random.seed(0)
         random_msg = np.random.randint(0, 255, size=10000).astype('uint8')
         encoded_data = cobs_encoding.encode_data(random_msg, escape_character=constants.SYNC_BYTE)
-        print '%r' % encoded_data
         buffer = encoded_data[:(len(encoded_data) / 2)] + buffer + encoded_data[(len(encoded_data) / 2):]
         filtered_buffer, sip_packets = hirate_receiving_methods.find_sip_packets_in_buffer(buffer)
-        print '%r' % filtered_buffer
         assert (filtered_buffer == encoded_data)
         assert (sip_packets == ['\xfa\xff\x01\x00\x00\x00\x01'])
 
     def false_start_test(self):
         buffer = '\xfa\xfa\xff\x01\x00\x00\x00\x01'
         filtered_buffer, sip_packets = hirate_receiving_methods.find_sip_packets_in_buffer(buffer)
-        print '%r' % filtered_buffer
-        print '%r' % sip_packets[0]
         assert (filtered_buffer == '\xfa')
         assert (sip_packets == ['\xfa\xff\x01\x00\x00\x00\x01'])
+
+    def insufficient_length_for_length_byte_test(self):
+        buffer = '\xfa\xff\x01'
+        filtered_buffer, sip_packets = hirate_receiving_methods.find_sip_packets_in_buffer(buffer)
+        assert (filtered_buffer == '\xfa\xff\x01')
+        assert (sip_packets == [])
+
+    def insufficient_length_for_length_byte_test(self):
+        buffer = '\xfa\xff\x01\x00\x00\x00'
+        filtered_buffer, sip_packets = hirate_receiving_methods.find_sip_packets_in_buffer(buffer)
+        assert (filtered_buffer == '\xfa\xff\x01\x00\x00\x00')
+        assert (sip_packets == [])
+
+    def bad_id_byte_test(self):
+        buffer = '\xfa\x99\x01\x00\x00\x00\x01'
+        filtered_buffer, sip_packets = hirate_receiving_methods.find_sip_packets_in_buffer(buffer)
+        assert (filtered_buffer == '\xfa\x99\x01\x00\x00\x00\x01')
+        assert (sip_packets == [])
