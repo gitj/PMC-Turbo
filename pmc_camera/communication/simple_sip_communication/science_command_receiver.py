cf490b03a30e553ada29aef73835d17ffa6c64c3
bjorn <bkjell@gmail.com>
2016-12-10 18:04:25 -0500

    Switching to if __name__ == "__main__" so that travis doesn't freak out (this is just sample code).

--- pmc_camera/communication/simple_sip_communication/science_command_receiver.py
+++ pmc_camera/communication/simple_sip_communication/science_command_receiver.py
@@ -5,11 +5,16 @@ IP = '192.168.1.137'
 
 PORT = 4001
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((IP, PORT))
 
-while True:
-    data = sock.recv(1024)
-    format_string = '%dB' % len(data)
-    d = struct.unpack(format_string, data)
-    print d
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((IP, PORT))
+
+    while True:
+        data = sock.recv(1024)
+        format_string = '%dB' % len(data)
+        d = struct.unpack(format_string, data)
+        print d
+
+if __name__ == "__main__":
+    main()
