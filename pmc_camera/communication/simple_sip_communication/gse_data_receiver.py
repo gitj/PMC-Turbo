2cb6781374fae8465ab424e2af430af53d54801d
bjorn <bkjell@gmail.com>
2016-12-10 18:04:25 -0500

    Switching to if __name__ == "__main__" so that travis doesn't freak out (this is just sample code).

--- pmc_camera/communication/simple_sip_communication/gse_data_receiver.py
+++ pmc_camera/communication/simple_sip_communication/gse_data_receiver.py
@@ -6,10 +6,17 @@ BAUDRATE = 115200
 
 ser = serial.Serial(USB_PORT_ADDRESS, baudrate=BAUDRATE)
 ser.timeout = 5
-while True:
-    data = ser.read(255)
 
-    format_string = '%dB' % len(data)
-    d = struct.unpack(format_string, data)
-    print d
-ser.close()
+
+def main():
+    while True:
+        data = ser.read(255)
+
+        format_string = '%dB' % len(data)
+        d = struct.unpack(format_string, data)
+        print d
+    ser.close()
+
+
+if __name__ == "__main__":
+    main()
