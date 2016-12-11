b5a74cbad70909369e36274a52d8d849b93ee178
bjorn <bkjell@gmail.com>
2016-12-11 18:48:29 -0500

    Put file declarations in __name__ == "__main__" since I haven't set up directory on travis.

--- pmc_camera/communication/run_camera_communicator.py
+++ pmc_camera/communication/run_camera_communicator.py
@@ -4,23 +4,6 @@ import logging
 import os
 import time
 
-logger = logging.getLogger('pmc_camera')
-default_handler = logging.StreamHandler()
-
-LOG_DIR = '/home/pmc/logs/camera_communicator'
-filename = os.path.join(LOG_DIR, (time.strftime('%Y-%m-%d_%H%M%S.txt')))
-default_filehandler = logging.FileHandler(filename=filename)
-
-message_format = '%(levelname)-8.8s %(asctime)s - %(name)s.%(funcName)s:%(lineno)d  %(message)s'
-default_formatter = logging.Formatter(message_format)
-default_handler.setFormatter(default_formatter)
-default_filehandler.setFormatter(default_formatter)
-logger.addHandler(default_handler)
-logger.addHandler(default_filehandler)
-logger.setLevel(logging.DEBUG)
-
-UPLINK_IP, UPLINK_PORT, DOWNLINK_IP, DOWNLINK_PORT = '192.168.1.30', 4001, '192.168.1.54', 4001
-
 
 def main():
     c = camera_communicator.Communicator(0)
@@ -31,4 +14,20 @@ def main():
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger('pmc_camera')
+    default_handler = logging.StreamHandler()
+
+    LOG_DIR = '/home/pmc/logs/camera_communicator'
+    filename = os.path.join(LOG_DIR, (time.strftime('%Y-%m-%d_%H%M%S.txt')))
+    default_filehandler = logging.FileHandler(filename=filename)
+
+    message_format = '%(levelname)-8.8s %(asctime)s - %(name)s.%(funcName)s:%(lineno)d  %(message)s'
+    default_formatter = logging.Formatter(message_format)
+    default_handler.setFormatter(default_formatter)
+    default_filehandler.setFormatter(default_formatter)
+    logger.addHandler(default_handler)
+    logger.addHandler(default_filehandler)
+    logger.setLevel(logging.DEBUG)
+
+    UPLINK_IP, UPLINK_PORT, DOWNLINK_IP, DOWNLINK_PORT = '192.168.1.30', 4001, '192.168.1.54', 4001
     main()
