914c94c7dd2544e9b5769271663e321aa33c308f
bjorn <bkjell@gmail.com>
2016-12-18 17:12:07 -0500

    Still work in progress. Captures images and gets packets from buffer. Resilient to bad CRCs.

---
+++ pmc_camera/communication/gse_receiver.py
@@ -0,0 +1,140 @@
+import serial
+import os
+import time
+import constants
+import packet_classes
+
+
+def get_new_data_into_buffer(time_loop, usb_port_address='/dev/ttyUSB0', baudrate=115200):
+    buffer = ''
+    ser = serial.Serial(usb_port_address, baudrate=baudrate)
+    ser.timeout = 1
+    start = time.time()
+    while (time.time() - start) < time_loop:
+        data = ser.read(10000)
+        buffer += data
+        # print '%r' % buffer
+    ser.close()
+    return buffer
+
+
+def get_gse_packets_from_buffer(buffer):
+    gse_packets = []
+    remainder = ''
+    while buffer:
+        idx = buffer.find(chr(0xFA))
+        if idx == -1:
+            # There's no GSE start byte in the buffer
+            remainder = buffer
+            break
+        try:
+            gse_packet = packet_classes.GSEPacket(buffer=buffer[idx:], greedy=False)
+            gse_packets.append(gse_packet)
+            # print '%r' % buffer[idx + gse_packet._header_length + gse_packet.payload_length + 1:]
+            buffer = buffer[idx + gse_packet._header_length + gse_packet.payload_length + 1:]
+        except packet_classes.PacketLengthError:
+            # This triggers when there are insufficient bytes to finish a GSEPacket
+            remainder = buffer[idx:]
+            break
+    return gse_packets, remainder
+
+
+def separate_hirate_and_lowrate_gse_packets(gse_packets):
+    lowrate_gse_packets = [packet for packet in gse_packets if packet.origin == 1]
+    hirate_gse_packets = [packet for packet in gse_packets if packet.origin == 2]
+    return hirate_gse_packets, lowrate_gse_packets
+
+
+def get_hirate_packets_from_buffer(buffer):
+    hirate_packets = []
+    remainder = ''
+    while buffer:
+        idx = buffer.find(chr(0x17))
+        if idx == -1:
+            print 'no start byte found'
+            remainder = buffer
+            break
+        try:
+            hirate_packet = packet_classes.HiratePacket(buffer=buffer[idx:])
+            hirate_packets.append(hirate_packet)
+            buffer = buffer[idx + hirate_packet._header_length + hirate_packet.payload_length + 2:]
+        except packet_classes.PacketLengthError as e:
+            print str(e)
+            # This triggers when there are insufficient bytes to finish a GSEPacket.
+            # This is common - usually just needs to wait for more data.
+            remainder = buffer[idx:]
+            break
+        except packet_classes.PacketChecksumError as e:
+            print str(e)
+            # This comes up occasionally - i need to think about how to handle this
+            # Probably just toss the packet... which is what I do already.
+            remainder = buffer[idx + 6 + 1000 + 2:]
+            # This is hardcoded right now because I know these lengths... I need to fix this in the future
+            # The problem is I can't use hirate_packet.payload_length, etc. since the packet never gets formed
+            # It raises this exception instead - possible to pass arguments in exception?
+            break
+    return hirate_packets, remainder
+
+
+def main():
+    timestamp = time.strftime('%Y-%m-%d_%H%M%S')
+    generic_path = './gse_receiver_data'
+    path = os.path.join(generic_path, timestamp)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    raw_filename = os.path.join(path, 'raw.log')
+    lowrate_file_name = os.path.join(path, 'lowrate.log')
+
+    files = {}
+    hirate_remainder = ''
+
+    while True:
+        buffer = get_new_data_into_buffer(1)
+        if not buffer:
+            continue
+        f = open(raw_filename, 'ab+')
+        f.write(buffer)
+        f.close()
+
+        gse_packets, gse_remainder = get_gse_packets_from_buffer(buffer)
+        gse_hirate_packets, gse_lowrate_packets = separate_hirate_and_lowrate_gse_packets(gse_packets)
+
+        f = open(raw_filename, 'ab+')
+        for packet in gse_lowrate_packets:
+            # f.write(packet.to_buffer())
+            print 'lowrate_received'
+        f.close()
+
+        gse_hirate_buffer = ''
+        for packet in gse_hirate_packets:
+            gse_hirate_buffer += packet.payload
+        hirate_packets, remainder = get_hirate_packets_from_buffer(hirate_remainder + gse_hirate_buffer)
+        for packet in hirate_packets:
+            print packet.file_id
+            print packet.packet_number, packet.total_packet_number
+            print
+        hirate_remainder = remainder
+
+
+'''
+        if hirate_packets:
+            for packet in hirate_packets:
+                if packet.file_id in files.keys():
+                    files[packet.file_id].append(packet)
+                else:
+                    files[packet.file_id] = [packet]
+            hirate_packets = []
+            for file_id in files.keys():
+                print file_id
+                print [packet.packet_number for packet in files[file_id]]
+                print files[file_id][0].total_packet_number
+                # sorted_packets = sorted(files[file_id], key=lambda k: k.packet_number)
+                # if [p.packet_number for p in sorted_packets] == range(sorted_packets[-1].packet_number):
+                ##    print 'full image received.'
+                # else:
+                #    print [packet.packet_number for packet in sorted_packets]
+                #    print sorted_packets[0].total_packet_number
+                '''
+
+if __name__ == "__main__":
+    main()
