71c8ab074f56a2d66ee22c80f78d64cde55106f7
bjorn <bkjell@gmail.com>
2016-12-17 20:02:19 -0500

    Changed HiratePacket to follow GSEPacket format - added CRC checking and _repr_() methods.

--- pmc_camera/communication/packet_classes.py
+++ pmc_camera/communication/packet_classes.py
@@ -2,6 +2,7 @@ import struct
 import numpy as np
 from PyCRC.CRCCCITT import CRCCCITT
 
+
 def get_checksum(data):
     return int(np.sum(np.frombuffer(data, dtype='uint8'), dtype='uint8'))
 
@@ -9,11 +10,13 @@ def get_checksum(data):
 def get_crc(data):
     return CRCCCITT().calculate(data)
 
+
 class PacketError(RuntimeError):
     """
     General packet exception
     """
     pass
+
 
 class PacketLengthError(PacketError):
     """
@@ -21,17 +24,20 @@ class PacketLengthError(PacketError):
     """
     pass
 
+
 class PacketChecksumError(PacketError):
     """
     Exception for packets that don't pass checksum or CRC tests
     """
     pass
 
+
 class GSEPacket(object):
     _header_format_string = '>4B1H'
     _valid_start_byte = 0xFA
     _valid_sync2_bytes = [0xFA, 0xFB, 0xFC, 0xFD, 0xFF]
-    def __init__(self,buffer=None,sync2_byte=None,origin=None,payload=None,greedy=True):
+
+    def __init__(self, buffer=None, sync2_byte=None, origin=None, payload=None, greedy=True):
         """
         GSE style packet, received from the GSE and passed to the ground computer.
 
@@ -73,7 +79,12 @@ class GSEPacket(object):
             self.payload_checksum = get_checksum(payload)
             self.start_byte = self._valid_start_byte
 
-    def from_buffer(self,buffer, greedy=True):
+    def _repr_(self):
+        return 'Sync2: %d \n Origin: %d \n Payload Length %d \n First 10 bytes: %s' % (self.sync2_byte,
+                                                                                       self.origin, self.payload_length,
+                                                                                       self.payload[:10])
+
+    def from_buffer(self, buffer, greedy=True):
         """
         Decode and validate the given buffer and update the class attributes accordingly
 
@@ -87,22 +98,22 @@ class GSEPacket(object):
         """
         if len(buffer) < self._minimum_buffer_length:
             raise PacketLengthError("Buffer of length %d is too short to contain a packet (minimum length is %d)" %
-                                       (len(buffer),self._minimum_buffer_length))
+                                    (len(buffer), self._minimum_buffer_length))
         self.start_byte, self.sync2_byte, self.origin, _, self.payload_length = struct.unpack(
             self._header_format_string, buffer[:self._header_length])
         if greedy:
             checksum_index = -1
         else:
-            checksum_index = self._header_length+self.payload_length+1
+            checksum_index = self._header_length + self.payload_length + 1
         payload = buffer[self._header_length:checksum_index]
         if len(payload) != self.length:
-            raise PacketLengthError("Payload length %d does not match length field value %d" %(len(payload),
+            raise PacketLengthError("Payload length %d does not match length field value %d" % (len(payload),
                                                                                                 self.length))
 
         payload_checksum = get_checksum(payload)
         if payload_checksum != ord(buffer[checksum_index]):
             raise PacketChecksumError("Payload checksum %d does not match checksum field value %d" %
-                                      (payload_checksum,ord(buffer[checksum_index])))
+                                      (payload_checksum, ord(buffer[checksum_index])))
         self.payload = payload
         self.payload_checksum = payload_checksum
         if self.sync2_byte not in self._valid_sync2_bytes:
@@ -123,48 +134,100 @@ class GSEPacket(object):
                              self.payload_length)
         return header + self.payload + chr(self.payload_checksum)
 
-class SIPPacket():
-    def __init__(self):
-        self.header_format_string = '>4B1H'
-        self.header_length = 6
-        return
 
-    def from_buffer(self, buffer):
-        self.start_byte, self.sync2, self.origin, _, self.length = struct.unpack(self.header_format_string,
-                                                                                 buffer[:self.header_length])
-        self.data = buffer[self.header_length:-1]
-        self.checksum = ord(buffer[-1])
+class HiratePacket(object):
+    _header_format_string = '>4B1H'
+    _valid_start_byte = 0x17
+
+    def __init__(self, buffer=None, file_id=None, packet_number=None, total_packet_number=None, payload=None,
+                 greedy=True):
+        """
+        Hirate packet. We break data into chunks and send them to the SIP in this packet format.
+
+        To decode a packet, use as HiratePacket(data), and access the payload attribute.
+
+        To construct a packet, use as
+        packet = GSEPacket(file_id=101,packet_number=2,total_packet_number=4,payload="hello world")
+        data = packet.to_buffer()
+
+        Parameters
+        ----------
+        buffer : string
+            A data buffer to decode as a packet
+        file_id : uint8
+            file_id assigned to file when breaking it up to send.
+        packet_number : uint8
+            Nth packet in file with file_id
+        total_packet_number : uint8
+            Total number of packets in file
+        payload : string
+            Bytes to package into the packet
+        greedy : bool
+            Only used for decoding.
+            If true, assume the buffer contains exactly one packet.
+            If false, use the length field of the packet to decide how much of the buffer to interpret.
+        """
+        self._header_length = struct.calcsize(self._header_format_string)
+        self._minimum_buffer_length = self._header_length + 1
+        if buffer is not None:
+            self.from_buffer(buffer, greedy=greedy)
+        else:
+            self.file_id = file_id
+            self.packet_number = packet_number
+            self.total_packet_number = total_packet_number
+            self.payload = payload
+            self.payload_length = len(payload)
+            self.payload_crc = get_crc(payload)
+            self.start_byte = self._valid_start_byte
+
+    def _repr_(self):
+        return 'File_id: %d \n Packet Number %d of %d \n First 10 bytes: %s' % (
+            self.file_id, self.packet_number, self.total_packet_number, self.payload[:10])
+
+    def from_buffer(self, buffer, greedy=True):
+        """
+        Decode and validate the given buffer and update the class attributes accordingly
+
+        Parameters
+        ----------
+        buffer : str
+            buffer to decode as a packet
+        greedy : bool
+            If true, assume the buffer contains exactly one packet.
+            If false, use the length field of the packet to decide how much of the buffer to interpret.
+        """
+        if len(buffer) < self._minimum_buffer_length:
+            raise PacketLengthError("Buffer of length %d is too short to contain a packet (minimum length is %d)" %
+                                    (len(buffer), self._minimum_buffer_length))
+        self.start_byte, self.file_id, self.packet_number, self.total_packet_number, self.payload_length = struct.unpack(
+            self._header_format_string, buffer[:self._header_length])
+        if greedy:
+            crc_index = -1
+        else:
+            crc_index = self._header_length + self.payload_length + 1
+        payload = buffer[self._header_length:checksum_index]
+        if len(payload) != self.length:
+            raise PacketLengthError("Payload length %d does not match length field value %d" % (len(payload),
+                                                                                                self.length))
+
+        payload_crc = get_crc(payload)
+        if payload_crc != ord(buffer[crc_index]):
+            raise PacketChecksumError("Payload CRC %d does not match CRC field value %d" %
+                                      (payload_crc, ord(buffer[crc_index])))
+        self.payload = payload
+        self.payload_crc = payload_crc
 
     def to_buffer(self):
-        header_buff = struct.pack(self.header_format_string, self.start_byte, self.sync2, self.origin, 0, self.length)
-        return header_buff + self.data + chr(get_checksum(self.data))
+        """
+        Construct the packet string
 
-
-class HiratePacket():
-    def __init__(self):
-        self.header_format_string = '>4B1H'
-        self.header_length = 6
-
-    def from_arguments(self, start_byte, file_id, packet_number, total_packet_number, data):
-        self.start_byte = start_byte
-        self.file_id = file_id
-        self.packet_number = packet_number
-        self.total_packet_number = total_packet_number
-        self.data = data
-        self.length = len(data)
-        self.crc = get_crc(data)
-
-    def from_buffer(self, buffer):
-        self.start_byte, self.file_id, self.packet_number, self.total_packet_number, self.length = struct.unpack(
-            self.header_format_string,
-            buffer[:self.header_length])
-        self.data = buffer[self.header_length:-2]
-        if len(self.data) != self.length:
-            raise AttributeError('Data length and predicted data length do not match.')
-        self.crc, = struct.unpack('>1H', buffer[-2:])
-
-    def to_buffer(self):
-        header_buff = struct.pack(self.header_format_string, self.start_byte, self.file_id, self.packet_number,
-                                  self.total_packet_number, self.length)
-        crc_buff = struct.pack('>1H', self.crc)
-        return header_buff + self.data + crc_buff
+        Returns
+        -------
+        buffer : string containing the packet
+        """
+        assert (self.file_id is not None) and (self.packet_number is not None) and (
+            self.total_packet_number is not None) and (self.payload is not None)
+        header = struct.pack(self._header_format_string, self.start_byte, self.file_id, self.packet_number,
+                             self.total_packet_number,
+                             self.payload_length)
+        return header + self.payload + chr(self.payload_crc)
