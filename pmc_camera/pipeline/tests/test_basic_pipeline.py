382b67eb9d666a7f0d428dd8d4b00e50ab841510
gitj <glenn.caltech@gmail.com>
2016-12-28 13:09:34 -0500

    debugging pipeline freezing on travis...

--- pmc_camera/pipeline/tests/test_basic_pipeline.py
+++ pmc_camera/pipeline/tests/test_basic_pipeline.py
@@ -6,13 +6,19 @@ def test_pipeline_runs():
     thread = threading.Thread(target=bpl.run_pyro_loop)
     thread.daemon=True
     thread.start()
+    print "pyro thread started"
     time.sleep(1)
     bpl.get_status()
+    print "got status"
     tag = bpl.send_camera_command("ExposureTimeAbs","10000")
+    print "sent command"
     name,value,result,gate_time = bpl.send_camera_command_get_result("ExposureTimeAbs","1000",timeout=2)
+    print "sent command, got result"
     name,value,result,gate_time = bpl.get_camera_command_result(tag)
+    print "got command result"
     time.sleep(1)
     bpl.close()
+    print "shut down"
 
 def test_pipeline_runs_no_disk():
     bpl = basic_pipeline.BasicPipeline(disks_to_use=['/tmp'],use_simulated_camera=True,default_write_enable=0)
