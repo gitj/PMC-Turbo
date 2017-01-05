74512b8dffe5d36ca78b170f2a1ec0268ce62d5a
gitj <glenn.caltech@gmail.com>
2017-01-04 22:30:28 -0500

    minor updates

--- pmc_camera/utils/run_pipeline.py
+++ pmc_camera/utils/run_pipeline.py
@@ -7,7 +7,7 @@ import signal
 
 if __name__ == "__main__":
     pmc_camera.utils.log.setup_stream_handler()
-    pmc_camera.utils.log.setup_file_handler('pipeline')
+    pmc_camera.utils.log.setup_file_handler()
     bpl = BasicPipeline(disks_to_use=['/data1','/data2','/data3','/data4'],default_write_enable=1)
     signal.signal(signal.SIGTERM,bpl.exit)
     bpl.run_pyro_loop()
