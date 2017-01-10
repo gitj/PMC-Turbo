435780cce61a7fbfc3bf66d2ee0a16d918177373
gitj <glenn.caltech@gmail.com>
2017-01-09 23:44:01 -0500

    more debugging

--- pmc_camera/pipeline/tests/test_basic_pipeline.py
+++ pmc_camera/pipeline/tests/test_basic_pipeline.py
@@ -28,7 +28,7 @@ def test_pipeline_runs():
     print "got command result"
     time.sleep(1)
     bpl.close()
-    print subprocess.check_output(('ls -R %s' % tempdir),shell=True)
+    print subprocess.check_output(('ls -Rhl %s' % tempdir),shell=True)
     shutil.rmtree(tempdir)
     print "shut down"
 
