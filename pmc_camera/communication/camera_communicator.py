06686d3d81420b7d05bc1218cb92ce8509ab1641
ckjellstrand <bkjell@gmail.com>
2017-02-05 00:15:09 -0500

    Checking peers and controller for objects, not just uris or proxies.

--- pmc_camera/communication/camera_communicator.py
+++ pmc_camera/communication/camera_communicator.py
@@ -52,7 +52,7 @@ class Communicator():
             try:
                 self.peers.append(Pyro4.Proxy(peer))
             except TypeError as e:
-                if not hasattr(peer, '_pyroUri'):
+                if not hasattr(peer, '_pyroUri') or not hasattr(peer, 'cam_id'):
                     raise e
                 else:
                     self.peers.append(peer)
@@ -61,7 +61,7 @@ class Communicator():
             try:
                 self.controller = Pyro4.Proxy(controller)
             except TypeError as e:
-                if not hasattr(controller, '_pyroUri'):
+                if not hasattr(controller, '_pyroUri') or not hasattr(controller, 'pipeline'):
                     raise e
                 else:
                     self.controller = controller
