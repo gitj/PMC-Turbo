3527a9e4f6f6e3920d9be8e2b83fee325c771f7c
gitj <glenn.caltech@gmail.com>
2017-02-07 22:18:05 -0500

    reenable blosc tests

--- pmc_turbo/pmc_camera/image_processing/tests/test_blosc_file.py
+++ pmc_turbo/pmc_camera/image_processing/tests/test_blosc_file.py
@@ -2,8 +2,9 @@ import multiprocessing as mp
 import os
 import shutil
 import tempfile
+from nose.tools import timed
 
-__test__ = False
+#__test__ = False
 
 import blosc
 import numpy as np
@@ -45,19 +46,19 @@ class TestBloscFiles(object):
         blosc_file.write_image_blosc(filename, image)
         image2,chunk2 = blosc_file.load_blosc_image(filename)
 
-
+    @timed(10)
     def test_blosc_multiprocessing(self):
         filename = os.path.join(self.temp_dir,'blah3.blosc')
         data = np.random.random_integers(0,2**14-1,size=(31440952//2,)).astype('uint16')
         p = mp.Process(target=blosc_file.write_image_blosc, args=(filename, data))
         p.start()
-        p.join(10)
+        p.join(2)
         print "one process, ok"
 
         filename = os.path.join(self.temp_dir,'blah4.blosc')
         p = mp.Process(target=write_random_blosc_file, args=(filename,))
         p.start()
-        p.join(10)
+        p.join(2)
         print "one process, type 2 ok"
 
         filename = os.path.join(self.temp_dir,'blah5.blosc')
@@ -66,8 +67,8 @@ class TestBloscFiles(object):
         p2 = mp.Process(target=write_random_blosc_file, args=(filename2,))
         p.start()
         p2.start()
-        p.join(10)
-        p2.join(10)
+        p.join(2)
+        p2.join(2)
         print "two process, type 2 ok"
 
 def write_random_blosc_file(filename):
