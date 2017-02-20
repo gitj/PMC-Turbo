5d1e7631176f35168c9d127cd842f06ff5792e37
gitj <glenn.caltech@gmail.com>
2017-02-19 22:53:38 -0500

    fix residual refactoring issues

--- scripts/python/run_pipeline.py
+++ scripts/python/run_pipeline.py
@@ -1,11 +1,11 @@
 import signal
 
 from pmc_turbo.camera.pipeline.basic_pipeline import BasicPipeline
-import pmc_turbo.pmc_camera.utils.log
+import pmc_turbo.utils.log
 
 if __name__ == "__main__":
-    pmc_turbo.pmc_camera.utils.log.setup_stream_handler()
-    pmc_turbo.pmc_camera.utils.log.setup_file_handler('pipeline')
+    pmc_turbo.utils.log.setup_stream_handler()
+    pmc_turbo.utils.log.setup_file_handler('pipeline')
     bpl = BasicPipeline(disks_to_use=['/data1','/data2','/data3','/data4'],default_write_enable=1)
     signal.signal(signal.SIGTERM,bpl.exit)
     bpl.run_pyro_loop()
