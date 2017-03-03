0169a1f90b7c2fc551c687f1a94920feda615247
gitj <glenn.caltech@gmail.com>
2017-03-03 15:47:07 -0500

    big refactor of SIP uplink packet parsing and associated tests fix long standing issue of sip packet being spit across multiple reads

--- pmc_turbo/communication/uplink_classes.py
+++ pmc_turbo/communication/uplink_classes.py
@@ -6,124 +6,84 @@ from pmc_turbo.communication import constants
 
 logger = logging.getLogger(__name__)
 
-
-# start_byte = chr(constants.SIP_START_BYTE)
-# end_byte = chr(constants.SIP_END_BYTE)
-
 class Uplink():
     def __init__(self, uplink_port):
         self.uplink_port = uplink_port
-        # sip_ip='192.168.1.137', sip_port=4001 in our experimental setup.
         socket_ = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         socket_.bind(('0.0.0.0', self.uplink_port))
         socket_.settimeout(0)
         self.uplink_socket = socket_
-        self.sip_leftover_buffer = None
+        self.sip_leftover_buffer = ''
 
     def get_sip_packets(self):
-        valid_packets = []
         try:
-            data = self.uplink_socket.recv(1024)
+            data = self.uplink_socket.recv(2000)
 
             logger.debug('Received bytes on uplink: %r' % data)
 
-            # buffer = self.sip_leftover_buffer + data
-            buffer = data
-            self.sip_leftover_buffer = ''
-            while buffer:
-                valid_packet, remainder = process_bytes(buffer)
-                logger.debug('Packet is: %r' % valid_packet)
-                if valid_packet:
-                    valid_packets.append(valid_packet)
-                    buffer = remainder
-                else:
-                    # logger.debug('Did not receive valid packet. Remainder is: %r' % remainder)
-                    self.sip_leftover_buffer = remainder
-                    break
-            return valid_packets
-        except socket.error as e:  # This should except a timeouterrror.
+            buffer = self.sip_leftover_buffer + data
+            packets, self.sip_leftover_buffer = get_sip_uplink_packets_from_buffer(buffer)
+            return packets
+        except socket.error as e:  # This should catch a timeouterrror.
             if e.errno != 11:
-                pass  # logger.exception(str(e))
-            return valid_packets
+                logger.exception("Unexpected exceception caught while receiving SIP packets")
+        return []
 
 
-def process_bytes(buffer, logger=None):
-    start_byte = chr(constants.SIP_START_BYTE)
-    end_byte = chr(constants.SIP_END_BYTE)
-    science_data_request_byte = chr(constants.SCIENCE_DATA_REQUEST_BYTE)
-    science_command_byte = chr(constants.SCIENCE_COMMAND_BYTE)
-    packet = None
-    remainder = buffer
+def get_sip_uplink_packets_from_buffer(buffer):
+    start_character = chr(constants.SIP_START_BYTE)
+    end_character = chr(constants.SIP_END_BYTE)
+    packets = []
+    remainder = ''
     while buffer:
-        idx = buffer.find(start_byte)
+        idx = buffer.find(start_character)
         if idx == -1:
             # This means a START_BYTE was not found
             # We are done processing - discard junk before first idx.
-            packet, remainder = None, ''
             break
-        if not len(buffer) > (idx + 1):
-            # Make sure the buffer is long enough...
-            packet = None
-            remainder = buffer[idx:]
+        else:
+            logger.debug("Found start byte at %d, advancing to this byte" % idx)
+            buffer = buffer[idx:]
+        if len(buffer) < 3:  # all valid packets are at least 3 bytes long, so we don't have enough bytes yet
+            remainder = buffer
             break
-        id_byte = buffer[idx + 1]
-        if not id_byte in [science_data_request_byte, science_command_byte]:
+        received_start_byte, id_byte = struct.unpack('>1B1B',buffer[:2])
+        if not id_byte in constants.VALID_SIP_IDS:
             # If the id_byte is not valid, we cut off the junk and continue the loop.
-            buffer = buffer[idx + 2:]
+            logger.error("Received unexpected SIP message ID byte %02X. Advancing 2 bytes.\nCurrent received buffer is:\n%r" % (id_byte, buffer))
+            buffer = buffer[2:]
             continue
-        if id_byte == science_data_request_byte:
-            # Science data request
-            if logger:
-                logger.debug('%d' % len(buffer[idx:]))
-            if len(buffer[idx:]) < 3:
-                # We are just missing the end byte - throw it in the remainder.
-                packet = None
-                remainder = buffer[idx:]
-                break
+        if id_byte == constants.SCIENCE_DATA_REQUEST_MESSAGE:
+            if end_character == buffer[2]:
+                logger.info("Found science data request packet")
+                packets.append(buffer[:3])
 
-            if buffer[idx + 2] == end_byte:
-                packet = buffer[idx:idx + 3]
-                remainder = buffer[idx + 3:]
-                break
             else:
-                packet = None
-                remainder = buffer[idx + 2:]
-                if remainder.find(start_byte) != -1:
-                    # If there is another start byte in the buffer, this is junk
-                    buffer = remainder
-                    continue
-                else:
-                    break
+                logger.error("Received science data request packet with invalid end byte.\nCurrently received buffer is:\n%r" % buffer)
+            logger.debug("Advancing 1 bytes to next packet")
+            buffer = buffer[1:]
+            continue
 
-        if id_byte == science_command_byte:
-            if logger:
-                logger.debug('Id byte 14 found')
-            if len(buffer[idx:]) < 3:
-                if logger:
-                    logger.debug('Length of buffer insufficient to contain length byte')
-                packet = None
-                remainder = buffer[idx:]
-                break
-            length, = struct.unpack('<1B', buffer[idx + 2])
-            if len(buffer[idx:]) < (3 + length + 1):
-                if logger:
-                    logger.debug('Length of buffer insufficient to contain full packet.')
-                # We are missing the rest of the packet. Throw it in the remainder.
-                packet = None
-                remainder = buffer[idx:]
+        elif id_byte == constants.SCIENCE_COMMAND_MESSAGE:
+            payload_length, = struct.unpack('>1B',buffer[2])
+            if len(buffer[3:]) < payload_length + 1:
+                logger.debug('Length of buffer %d insufficient to contain full science command packet with payload length %d.' % (len(buffer),
+                                                                                                                                  payload_length))
+                remainder = buffer
                 break
 
-            if buffer[idx + 3 + length] == end_byte:
-                if logger:
-                    logger.debug('Expected end byte is an end byte.')
-                packet = buffer[idx:idx + 3 + length + 1]
-                remainder = buffer[idx + 3 + length + 1:]
-                break
-            else:
-                if logger:
-                    logger.debug('Expected end byte is NOT an end byte.')
-                packet = None
-                remainder = buffer[idx + 3:]
-                buffer = remainder
+            if buffer[3 + payload_length] == end_character:
+                logger.info("Found science command packet")
+                packets.append(buffer[:3+payload_length+1])
+                buffer = buffer[3+payload_length+1:]
                 continue
-    return packet, remainder
+            else:
+                logger.error("Received science command packet with invalid end byte.\nCurrently received buffer is:\n%r" % buffer)
+                logger.debug('Advancing 1 byte')
+                buffer = buffer[1:] # presumed id or length byte could actually be a valid start byte, so don't throw it away.
+                continue
+        else:
+            logger.error("Received Unhandled SIP message with ID %02X.\nCurrent buffer is\n%r" % (id_byte,buffer))
+            logger.debug("Advancing 1 byte")
+            buffer = buffer[1:]
+    return packets, remainder
