1faf73c492f115ca302f434848dba5935e7269bb
gitj <glenn.caltech@gmail.com>
2017-03-07 14:43:06 -0500

    check /etc/hosts configuration

--- config/balloon/default_balloon.py
+++ config/balloon/default_balloon.py
@@ -52,7 +52,7 @@ c.Communicator.hirate_link_parameters = [('highrate', ('pmc-serial-0', 5002), 70
                                          ('openport', ('192.168.1.70', 4501), 10000),
                                          ('los', ('pmc-serial-2', 5004), 0)]
 
-## List of tuples - lowrate downlink address and lowrate uplink port.e.g.
+## List of tuples - name, lowrate downlink address and lowrate uplink port.e.g.
 #  [(("pmc-serial-1", 5001), 5001), ...]
 c.Communicator.lowrate_link_parameters = [('comm1', ("pmc-serial-1", 5001), 5001),
                                           ('comm2', ('pmc-serial-2', 5003), 5003)]
--- pmc_turbo/housekeeping/check_system_settings.py
+++ pmc_turbo/housekeeping/check_system_settings.py
@@ -2,6 +2,7 @@
 This file is intended to be run manually with nosetests
 """
 from subprocess import check_output
+import socket
 
 def test_sudo_no_password():
     try:
@@ -11,4 +12,18 @@ def test_sudo_no_password():
     except Exception:
         pass
     assert False
+
+def test_hostname():
+    ip = socket.gethostbyname(socket.gethostname())
+    assert ip.startswith('192.168.1')
+
+def test_hosts():
+    hosts = [('pmc-camera-%d' % k) for k in range(8)]
+    hosts += [('pmc-charge-controller-%d' %k) for k in range(2)]
+    hosts += [('pmc-serial-%d' %k) for k in range(2)]
+    for host in hosts:
+        ip = socket.gethostbyname(host)
+        assert ip.startswith('192.168.1')
+
+
 
