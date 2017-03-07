c1490331797fe8187fb0a50c60bf835ea0379e2a
gitj <glenn.caltech@gmail.com>
2017-03-07 12:13:01 -0500

    add smoke test for election code

--- pmc_turbo/communication/election/tests/test_election.py
+++ pmc_turbo/communication/election/tests/test_election.py
@@ -3,11 +3,14 @@ import threading
 import time
 from pmc_turbo.communication.election.bully_election import ConcensusBullyPlayer, SimpleBullyPlayer
 import Pyro4
+import random
 
 Pyro4.config.SERIALIZER = 'pickle'
 Pyro4.config.SERIALIZERS_ACCEPTED = ['pickle']
 Pyro4.config.SERVERTYPE = 'multiplex'
 Pyro4.config.COMMTIMEOUT = 0.1
+import logging
+logger = logging.getLogger(__name__)
 
 @Pyro4.expose
 class DummyPeer():
@@ -55,18 +58,14 @@ class DummyPeer():
     def notification_of_new_leader(self,leader_id):
         self.player.notification_of_new_leader(leader_id)
 
-if __name__ == "__main__":
-    import pmc_turbo.utils.log
-    import logging
+def test_bully_election(interval = 0.1):
     import random
-    logger = pmc_turbo.utils.log.pmc_turbo_logger
-    pmc_turbo.utils.log.setup_stream_handler(logging.DEBUG)
 
     player_class = SimpleBullyPlayer
 
     num_peers = 4
     address_book = dict([(k,('localhost',57430+k)) for k in range(num_peers)])
-    peers = [DummyPeer(k,address_book,player_class=player_class) for k in range(num_peers)]
+    peers = [DummyPeer(k,address_book,player_class=player_class,main_loop_interval=interval) for k in range(num_peers)]
     for peer in peers:
         peer.ping_value = False
     indexes = range(num_peers)
@@ -75,25 +74,32 @@ if __name__ == "__main__":
     for k in indexes:
         peer = peers[k]
         logger.info("starting peer %d" % k)
-        time.sleep(random.random())
+        time.sleep(random.random()*interval)
         peer.start_player()
         peer.ping_value = True
     logger.info("waiting for a few rounds")
-    time.sleep(5)
+    time.sleep(5*interval)
     for peer in peers:
         logger.info("peer %d has leader %d" % (peer.player.id,peer.player.leader_id))
     logger.info("killing player 0")
     peers[0].ping_value = False
     peers[0].daemon.shutdown()
-    time.sleep(5)
+    time.sleep(5*interval)
     logger.info("ressurecting player 0")
     peers[0].player.leader_id = None
     peers[0].ping_value = True
-    time.sleep(5)
+    time.sleep(5*interval)
     logger.info("shutting down")
     for peer in peers:
         peer.exit = True
         peer.daemon.shutdown()
-    time.sleep(3)
+    time.sleep(3*interval)
     logger.info("done")
+
+if __name__ == "__main__":
+    import pmc_turbo.utils.log
+    import logging
+    logger = pmc_turbo.utils.log.pmc_turbo_logger
+    pmc_turbo.utils.log.setup_stream_handler(logging.DEBUG)
+    test_bully_election(1)
 
