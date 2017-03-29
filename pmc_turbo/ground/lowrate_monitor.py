53b371f2edbe1196ee6948b3f0661b65baab9cf2
gitj <glenn.caltech@gmail.com>
2017-03-28 21:55:40 -0400

    add lowrate data monitor

---
+++ pmc_turbo/ground/lowrate_monitor.py
@@ -0,0 +1,60 @@
+import bisect
+import os
+import glob
+from collections import OrderedDict
+import logging
+logger = logging.getLogger(__name__)
+import pmc_turbo.communication.short_status
+from pmc_turbo.ground.ground_configuration import GroundConfiguration
+
+
+class LowrateMonitor(GroundConfiguration):
+    def __init__(self,**kwargs):
+        super(LowrateMonitor,self).__init__(**kwargs)
+        self.gse_root_dir = self.find_latest_gse_dir()
+        self.lowrate_data = OrderedDict()
+        self.by_message_id = {}
+        self.bad_files = set()
+        self.update()
+
+    def find_latest_gse_dir(self):
+        dirs = glob.glob(os.path.join(self.root_data_path,'2*'))
+        dirs.sort()
+        logger.debug("Found gse dir %s" % dirs[-1])
+        return dirs[-1]
+
+    @property
+    def message_ids(self):
+        return self.by_message_id.keys()
+
+    def latest(self,message_id):
+        latest =  self.lowrate_data[self.by_message_id[message_id][-1]]
+        logger.debug("latest file for message id %d is %s" % (message_id, self.by_message_id[message_id][-1]))
+        return latest
+
+    def update(self):
+        filenames = glob.glob(os.path.join(self.gse_root_dir,'*/lowrate/*'))
+        filenames.sort()
+        logger.debug("Found %d total filenames" % len(filenames))
+        added = 0
+        for filename in filenames[::-1]:
+            if filename in self.lowrate_data:
+                if added:
+                    logger.info("added %d files" % added)
+                return
+            if filename in self.bad_files:
+                continue
+            try:
+                values = pmc_turbo.communication.short_status.load_short_status_from_file(filename).values
+            except Exception:
+                logger.exception("Failed to open %s" % filename)
+                self.bad_files.add(filename)
+                continue
+            message_id = values['message_id']
+            current_list = self.by_message_id.get(message_id,[])
+            bisect.insort(current_list,filename)
+            self.by_message_id[message_id] = current_list
+            self.lowrate_data[filename] = values
+            added += 1
+        if added:
+            logger.info("added %d files" % added)
---
+++ ptgui/lowrate_monitor_gui.py
@@ -0,0 +1,66 @@
+import time
+from pyqtgraph.Qt import QtCore,QtGui
+from pmc_turbo.ground.lowrate_monitor import LowrateMonitor
+from pmc_turbo.communication.short_status import ShortStatusLeader
+import logging
+logger = logging.getLogger(__name__)
+
+class LowrateMonitorWidget(QtGui.QLabel):
+    def __init__(self,lowrate_monitor,message_id,num_columns = 2, parent=None):
+        super(LowrateMonitorWidget,self).__init__(parent)
+        self.lowrate_monitor = lowrate_monitor
+        self.message_id = message_id
+        self.num_columns=num_columns
+        self.update_display()
+    def update_display(self):
+        #self.lowrate_monitor.update()
+        try:
+            values = self.lowrate_monitor.latest(self.message_id)
+        except KeyError:
+            logger.debug("No info for %d" % self.message_id)
+            return
+        if (time.time() - values['timestamp']) < 60:
+            logger.debug("data for %d is fresh" % self.message_id)
+            result = '<b>'
+            end = '</b>'
+        else:
+            result = ''
+            end = ''
+        result = result + '<table>'
+        end =  '</table>' +end
+        items = values.items()
+        while items:
+            row = ''.join([('<td>%s</td><td>%s</td>' % item) for item in items[:self.num_columns]])
+            row = '<tr>'+ row + '</tr>'
+            result = result + row
+            items = items[self.num_columns:]
+        self.setText(result+end)
+        logger.debug("Updated %d" % self.message_id)
+
+if __name__ == "__main__":
+    from pmc_turbo.utils import log
+    log.setup_stream_handler(log.logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(log.default_handler)
+    app  = QtGui.QApplication([])
+    widget = QtGui.QWidget()
+    layout = QtGui.QGridLayout()
+    widget.setLayout(layout)
+    lrm = LowrateMonitor()
+    leader = LowrateMonitorWidget(lrm,ShortStatusLeader.LEADER_MESSAGE_ID,parent=widget)
+    layout.addWidget(leader,0,0)
+    widgets = [leader]
+    for row in range(4):
+        for column in range(2):
+            w = LowrateMonitorWidget(lrm,row*2+column,parent=widget)
+            layout.addWidget(w,row+1,column)
+            widgets.append(w)
+    def update():
+        lrm.update()
+        for w in widgets:
+            w.update_display()
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update)
+    timer.start(1000)
+    widget.show()
+    app.exec_()
