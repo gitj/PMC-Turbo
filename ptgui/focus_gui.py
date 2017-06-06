9374c442c056ac9a7d4bd7b3e9c5cb12d428aaa5
bjorn <bkjell@gmail.com>
2017-06-06 12:30:00 -0400

    Focus gui update

--- ptgui/focus_gui.py
+++ ptgui/focus_gui.py
@@ -10,84 +10,182 @@ root_path = '/'
 proxy = Pyro4.Proxy('PYRO:controller@0.0.0.0:50001')
 
 
-def toolbtnpressed(a):
-    print a.text()
-
-    if a.text() == 'Increase_Focus':
-        # proxy.increase_focus
-        # for example
-        proxy.send_arbitrary_camera_command("EFLensFocus", argument_string)
-
-
-def increase_focus_button_press():
-    # focus_step = self.current_focus + self.focus_increment
-    # try
-    # proxy.set_focus(self, focus_step)
-    # self.current_focus =
-    # except
-    #
-    #
-    print 'Increase focus'
-
-
-def decrease_focus_button_press():
-    # focus_step = self.current_focus - self.focus_increment
-    # try
-    # proxy.set_focus(self, focus_step)
-    # self.current_focus =
-    # except
-    #
-    #
-    print 'Decrease focus'
-
-
-def increase_exposure_button_press():
-    # exposure_time_us = self.current_exposure_time_us + self.exposure_increment
-    # try
-    # proxy.set_exposure(self, focus_step)
-    # self.current_exposure =
-    # except
-    #
-    #
-    print 'Increase exposure'
-
-
-def decrease_exposure_button_press():
-    # exposure_time_us = self.current_exposure_time_us - self.exposure_increment
-    # try
-    # proxy.set_exposure(self, focus_step)
-    # self.current_exposure =
-    # except
-    #
-    #
-    print 'Decrease exposure'
-
-
 def update(window):
     # update both image viewer and realtimevalues.
     return
 
 
-class MyToolBar(QtGui.QToolBar):
-    def __init__(self, *args, **kwargs):
-        super(MyToolBar, self).__init__(*args, **kwargs)
-        increase_focus = QtGui.QAction("Increase Focus", win)
-        decrease_focus = QtGui.QAction("Decrease Focus", win)
-        increase_exposure_time = QtGui.QAction("Increase Exposure Time", win)
-        decrease_exposure_time = QtGui.QAction("Decrease Exposure Time", win)
+class GUIWrapper():
+    def __init__(self, proxy=True):
+        self.app = QtGui.QApplication([])
 
-        increase_focus.triggered.connect(increase_focus_button_press)
-        decrease_focus.triggered.connect(decrease_focus_button_press)
-        increase_exposure_time.triggered.connect(increase_exposure_button_press)
-        decrease_exposure_time.triggered.connect(decrease_exposure_button_press)
+        if proxy:
+            self.proxy = Pyro4.Proxy('PYRO:controller@0.0.0.0:50000')
+        else:
+            self.proxy = None
+        self.window = QtGui.QMainWindow()
+        self.window.resize(800, 800)
+        imv = pgview.MyImageView()
+        self.window.setCentralWidget(imv)
+
+        self.toolbar = MyToolBar(self)
+        self.window.addToolBar(QtCore.Qt.RightToolBarArea, self.toolbar)
+
+        self.mywidget = RealTimeValues()
+        self.window.addDockWidget(QtCore.Qt.TopDockWidgetArea, self.mywidget)
+
+        self.focus_step = 10
+        self.exposure_step = 10e3
+        self.focus = 2000
+        self.exposure = 100e3
+
+    def increase_focus_button_press(self):
+        focus_step = self.focus + self.focus_step
+        try:
+            if self.proxy:
+                self.proxy.set_focus(self, focus_step)
+            self.focus = focus_step
+            print 'Increased focus to %d' % self.focus
+        except Exception as e:
+            print e
+
+    def decrease_focus_button_press(self):
+        focus_step = self.focus - self.focus_step
+        try:
+            if self.proxy:
+                self.proxy.set_focus(self, focus_step)
+            self.focus = focus_step
+            print 'Decreased focus to %d' % self.focus
+        except Exception as e:
+            print e
+
+    def increase_exposure_button_press(self):
+        exposure_us = self.exposure + self.exposure_step
+        try:
+            if self.proxy:
+                self.proxy.set_exposure(self, exposure_us)
+            self.exposure = exposure_us
+            print 'Increased exposure to %d' % self.exposure
+        except Exception as e:
+            print e
+
+    def decrease_exposure_button_press(self):
+        exposure_us = self.exposure - self.exposure_step
+        try:
+            if self.proxy:
+                self.proxy.set_exposure(self, exposure_us)
+            self.exposure = exposure_us
+            print 'Increased exposure to %d' % self.exposure
+        except Exception as e:
+            print e
+
+    def change_focus_step(self, focus_step):
+        self.focus_step = focus_step
+        print 'Changed focus step to %d' % self.focus_step
+
+    def change_exposure_step(self, exposure_step):
+        self.exposure_step = exposure_step
+        print 'Changed exposure step to %d' % self.exposure_step
+
+    def change_focus(self, focus):
+        try:
+            if self.proxy:
+                self.proxy.set_focus(self, focus)
+            self.focus = focus
+            print 'Changed focus to %d' % self.focus
+        except Exception as e:
+            print e
+
+    def change_exposure(self, exposure):
+        try:
+            if self.proxy:
+                self.proxy.set_exposure(self, exposure)
+            self.exposure = exposure
+            print 'Changed exposure to %d' % self.exposure
+        except Exception as e:
+            print e
+
+
+class MyToolBar(QtGui.QToolBar):
+    def __init__(self, guiwrapper, *args, **kwargs):
+        super(MyToolBar, self).__init__(*args, **kwargs)
+        self.guiwrapper = guiwrapper
+        self.setup_layout()
+
+    def setup_layout(self):
+        increase_focus = QtGui.QAction("Increase Focus", self.guiwrapper.window)
+        decrease_focus = QtGui.QAction("Decrease Focus", self.guiwrapper.window)
+        increase_exposure_time = QtGui.QAction("Increase Exposure Time", self.guiwrapper.window)
+        decrease_exposure_time = QtGui.QAction("Decrease Exposure Time", self.guiwrapper.window)
+
+        increase_focus.triggered.connect(self.guiwrapper.increase_focus_button_press)
+        decrease_focus.triggered.connect(self.guiwrapper.decrease_focus_button_press)
+        increase_exposure_time.triggered.connect(self.guiwrapper.increase_exposure_button_press)
+        decrease_exposure_time.triggered.connect(self.guiwrapper.decrease_exposure_button_press)
+
+        self.focus_step_edit = QtGui.QLineEdit()
+        self.focus_step_edit.returnPressed.connect(self.change_focus_step)
+
+        self.exposure_step_edit = QtGui.QLineEdit()
+        self.exposure_step_edit.returnPressed.connect(self.change_exposure_step)
+
+        step_increment_widget = QtGui.QWidget()
+        step_increment_layout = QtGui.QGridLayout()
+
+        focus_step_label = QtGui.QLabel()
+        focus_step_label.setText('Focus Step: ')
+        step_increment_layout.addWidget(focus_step_label, 0, 0)
+        step_increment_layout.addWidget(self.focus_step_edit, 0, 1)
+
+        exposure_step_label = QtGui.QLabel()
+        exposure_step_label.setText('Exposure Step: ')
+        step_increment_layout.addWidget(exposure_step_label, 1, 0)
+        step_increment_layout.addWidget(self.exposure_step_edit, 1, 1)
+        step_increment_widget.setLayout(step_increment_layout)
+
+        absolute_widget = QtGui.QWidget()
+        absolute_layout = QtGui.QGridLayout()
+
+        self.focus_edit = QtGui.QLineEdit()
+        self.focus_edit.returnPressed.connect(self.change_focus)
+        self.exposure_edit = QtGui.QLineEdit()
+        self.exposure_edit.returnPressed.connect(self.change_exposure)
+
+        focus_label = QtGui.QLabel()
+        focus_label.setText('Set Focus: ')
+        absolute_layout.addWidget(focus_label, 0, 0)
+        absolute_layout.addWidget(self.focus_edit, 0, 1)
+
+        exposure_label = QtGui.QLabel()
+        exposure_label.setText('Set Exposure: ')
+        absolute_layout.addWidget(exposure_label, 1, 0)
+        absolute_layout.addWidget(self.exposure_edit, 1, 1)
+        absolute_widget.setLayout(absolute_layout)
 
         self.addAction(increase_focus)
         self.addAction(decrease_focus)
+
         self.addAction(increase_exposure_time)
         self.addAction(decrease_exposure_time)
 
-        # self.actionTriggered[QtGui.QAction].connect(toolbtnpressed)
-        #self.actionTriggered[increase_focus].connect(increase_focus_button_press)
+        self.addWidget(step_increment_widget)
+        self.addWidget(absolute_widget)
+
+    def change_focus_step(self):
+        self.guiwrapper.change_focus_step(self.focus_step_edit.text())
+        print self.focus_step_edit.text()
+
+    def change_exposure_step(self):
+        self.guiwrapper.change_exposure_step(self.exposure_step_edit.text())
+        print self.exposure_step_edit.text()
+
+    def change_focus(self):
+        self.guiwrapper.change_focus_step(self.focus_edit.text())
+        print self.focus_edit.text()
+
+    def change_exposure(self):
+        self.guiwrapper.change_focus_step(self.exposure_edit.text())
+        print self.exposure_edit.text()
 
 
 class RealTimeValues(QtGui.QDockWidget):
@@ -132,20 +230,8 @@ class RealTimeValues(QtGui.QDockWidget):
 
 
 if __name__ == "__main__":
-    app = QtGui.QApplication([])
-    # proxy = Pyro4.Proxy('')
-    win = QtGui.QMainWindow()
-    win.resize(800, 800)
-    imv = pgview.MyImageView()
-    win.setCentralWidget(imv)
-
-    tb = MyToolBar()
-    win.addToolBar(QtCore.Qt.RightToolBarArea, tb)
-
-    mywidget = RealTimeValues()
-    win.addDockWidget(QtCore.Qt.TopDockWidgetArea, mywidget)
-
-    win.show()
+    gw = GUIWrapper(proxy=False)
+    gw.window.show()
 
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
