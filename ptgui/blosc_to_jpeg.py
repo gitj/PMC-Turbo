55cbe35f8d86f6461cf341de3f687df696c436ab
ckjellstrand <bkjell@gmail.com>
2017-06-29 19:00:35 -0400

    minor convenience change

--- ptgui/blosc_to_jpeg.py
+++ ptgui/blosc_to_jpeg.py
@@ -2,9 +2,9 @@ import sys
 from pmc_turbo.camera.image_processing import blosc_file
 from pmc_turbo.camera.image_processing import jpeg
 
-def make_jpeg_from_blosc(filename):
+def make_jpeg_from_blosc(filename, **kwargs):
     img, chunk = blosc_file.load_blosc_image(filename)
-    img_jpeg = jpeg.simple_jpeg(img, scale_by=1, quality=100)
+    img_jpeg = jpeg.simple_jpeg(img, **kwargs)
     image_name = filename.split('/')[-1]
     with open('/home/pmc/%s.jpeg' % image_name, 'wb') as f:
         f.write(img_jpeg)
