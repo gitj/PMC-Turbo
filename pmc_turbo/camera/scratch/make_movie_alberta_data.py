d6ce2615e7b32db2f5ae13398769fe976d532720
bjorn <bkjell@gmail.com>
2017-07-13 14:27:34 -0400

    Added info widget to pgcamera Added option for section (incomplete) and downsampling (complete) to alberta make_movie

--- pmc_turbo/camera/scratch/make_movie_alberta_data.py
+++ pmc_turbo/camera/scratch/make_movie_alberta_data.py
@@ -13,7 +13,7 @@ imd = np.zeros((3232, 4864))
 dpi = 100
 
 
-def ani_frame(files, output_name):
+def ani_frame(files, output_name, downsample=True, section=None):
     frames = len(files)
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -21,7 +21,13 @@ def ani_frame(files, output_name):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
-    im = ax.imshow(scipy.ndimage.gaussian_filter(imd, 2)[::4, ::4], cmap=cm.gray, interpolation='nearest')
+    if section:
+        part = imd[section[0]:section[1], section[2]:section[3]]
+
+    if downsample:
+        im = ax.imshow(scipy.ndimage.gaussian_filter(imd, 2)[::4, ::4], cmap=cm.gray, interpolation='nearest')
+    else:
+        im = ax.imshow(scipy.ndimage.gaussian_filter(imd, 2), cmap=cm.gray, interpolation='nearest')
     fig.set_size_inches([12, 8])
     txt = ax.text(0.1, 0.9, 'time:', ha='left', va='top', color='yellow')
 
@@ -47,7 +53,10 @@ def ani_frame(files, output_name):
             return
         nblobs = 0
         # data = scipy.ndimage.gaussian_filter(imd,2)[::4,::4]
-        data = cv2.GaussianBlur(imall, (3, 3), 0)[::4, ::4]
+        if downsample:
+            data = cv2.GaussianBlur(imall, (3, 3), 0)[::4, ::4]
+        else:
+            data = cv2.GaussianBlur(imall, (3, 3), 0)
         im.set_data(data)
         mn = data.mean(dtype='float64')
         if mn < 1000:
@@ -59,10 +68,10 @@ def ani_frame(files, output_name):
         im.set_clim(0, mx)
         try:
             time_string = files[n].split('/')[-1].split('_')[1]
-            time_string = time_string[0:2]+':'+time_string[2:4]+':'+time_string[4:]
+            time_string = time_string[0:2] + ':' + time_string[2:4] + ':' + time_string[4:]
             txt.set_text(time_string)
         except Exception:
-             pass
+            pass
         print ("\r%d of %d %.1f minutes elapsed, %.1f minutes remaining, %d blobs" % (n, len(files), elapsed / 60,
                                                                                       (len(
                                                                                           files) - n) * time_per_frame / 60.,
@@ -72,8 +81,6 @@ def ani_frame(files, output_name):
 
     # legend(loc=0)
     ani = animation.FuncAnimation(fig, update_img, frames, interval=50)
-    #writer = animation.writers['ffmpeg'](fps=20, codec='h264')  # , bitrate=2**20)
-    # ffmpeg not installed on clouds
 
     writer = animation.writers['avconv'](fps=20, codec='h264')  # , bitrate=2**20)
 
@@ -81,23 +88,14 @@ def ani_frame(files, output_name):
     return ani
 
 
-# listing = """
-# 2016-10-26_135mm_trying_to_capture_PMClike_clouds_for_dave
-# 2016-10-26_135mm_trying_to_capture_PMClike_clouds_for_dave_.8ms
-# 2016-10-26_50mm_trying_to_capture_PMClike_clouds_for_dave_1ms_NDF
-# """
-# dirs = listing.splitlines()
-# import joblib
-# p = joblib.Parallel(n_jobs=3)
-def process_dir():
-    #files = glob.glob(os.path.join('/data/home/bjorn/alberta_data/raw_2017-07-01-camera-2/2017*'))
-    files = glob.glob(os.path.join('/data/home/bjorn/alberta_data/raw_2017-07-04_camera-1/2017-07-04_042010/2017*'))
+def process_dir(directory='/data/home/bjorn/alberta_data/raw_2017-07-04_camera-1/2017-07-04_042010/2017*',
+                output_name='/data/home/bjorn/movie_2017-07-04.mp4', downsample=True):
+    # files = glob.glob(os.path.join('/data/home/bjorn/alberta_data/raw_2017-07-01-camera-2/2017*'))
+    files = glob.glob(directory)
     files.sort()
     if len(files):
         print len(files[1::1]), files[14]
-        output_name = '/data/home/bjorn/movie_2017-07-04.mp4'
-        ani_frame(files[1::1], output_name)
+        ani_frame(files[1::1], output_name, downsample)
 
 
 process_dir()
-# p([joblib.delayed(process_dir)(x) for x in dirs])
--- ptgui/pgcamera.py
+++ ptgui/pgcamera.py
@@ -1,7 +1,7 @@
 import glob
 import os, sys
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore,QtGui
+from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 
 pg.setConfigOptions(imageAxisOrder='row-major')
@@ -10,67 +10,135 @@ from pmc_turbo.camera.pipeline.indexer import MergedIndex
 from pmc_turbo.ground.ground_configuration import GroundConfiguration
 from pmc_turbo.communication.file_format_classes import load_and_decode_file, JPEGFile
 
+
 class MyImageView(pg.ImageView):
-    def __init__(self,camera_id,*args,**kwargs):
-        #GroundConfiguration.__init__(**kwargs)
-        super(MyImageView,self).__init__(*args,**kwargs)
+    def __init__(self, camera_id, *args, **kwargs):
+        # GroundConfiguration.__init__(**kwargs)
+        super(MyImageView, self).__init__(*args, **kwargs)
         self.root_data_path = '/data/gse_data'
         self.camera_id = camera_id
-        data_dirs = glob.glob(os.path.join(self.root_data_path,'2*'))
+        data_dirs = glob.glob(os.path.join(self.root_data_path, '2*'))
         data_dirs.sort()
         print data_dirs[-1]
-        self.mi = MergedIndex('*',data_dirs=[data_dirs[-1]],index_filename='file_index.csv', sort_on=None)
+        self.mi = MergedIndex('*', data_dirs=[data_dirs[-1]], index_filename='file_index.csv', sort_on=None)
         self.last_index = 0
         self.update(-1, autoLevels=True, autoRange=True)
-    def update(self,index=-1,autoLevels=True,autoRange=True):
+
+    def update(self, index=-1, autoLevels=True, autoRange=True):
         self.mi.update()
         if self.camera_id is not None:
-            df = self.mi.df[self.mi.df.camera_id==self.camera_id]
+            df = self.mi.df[self.mi.df.camera_id == self.camera_id]
         else:
             df = self.mi.df
-        df = df[df.file_type==JPEGFile.file_type]
+        df = df[df.file_type == JPEGFile.file_type]
         if index == -1:
             index = df.index.max()
         try:
-            latest = df.iloc[df.index.get_loc(index,method='pad')]
-        except (IndexError,KeyError) as e:
-            print "invalid index",index, e
+            latest = df.iloc[df.index.get_loc(index, method='pad')]
+        except (IndexError, KeyError) as e:
+            print "invalid index", index, e
             return
         if index == self.last_index:
             return
-        self.last_index=index
+        self.last_index = index
         filename = latest['filename']
         print filename
         image_file = load_and_decode_file(filename)
-        image_data = image_file.image_array()/image_file.pixel_scale + image_file.pixel_offset
-        self.setImage(image_data,autoLevels=autoLevels,autoRange=autoRange, transform=QtGui.QTransform().rotate(-90))
+        image_data = image_file.image_array() / image_file.pixel_scale + image_file.pixel_offset
+        self.setImage(image_data, autoLevels=autoLevels, autoRange=autoRange, transform=QtGui.QTransform().rotate(-90))
 
     def keyPressEvent(self, ev):
         if ev.key() == QtCore.Qt.Key_N:
-            self.update(self.last_index+1)
+            self.update(self.last_index + 1)
             ev.accept()
         elif ev.key() == QtCore.Qt.Key_P:
-            self.update(self.last_index-1)
+            self.update(self.last_index - 1)
             ev.accept()
         elif ev.key() == QtCore.Qt.Key_L:
             self.update(-1)
             ev.accept()
 
-        super(MyImageView,self).keyPressEvent(ev)
+        super(MyImageView, self).keyPressEvent(ev)
+
+
+class InfoBar(QtGui.QDockWidget):
+    def __init__(self):
+        self.setWindowTitle("Info")
+        # self.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
+        self.setFeatures(QtGui.QDockWidget.DockWidgetVerticalTitleBar)
+        nested_widget = QtGui.QWidget()
+        layout = QtGui.QGridLayout()
+
+        frame_status_label = QtGui.QLabel('frame_status')
+        frame_id_label = QtGui.QLabel('frame_id')
+        frame_timestamp_ns = QtGui.QLabel('frame_timestamp_ns')
+        focus_step_label = QtGui.QLabel('focus_step')
+
+        aperture_stop_label = QtGui.QLabel('aperture_stop')
+        exposure_us_label = QtGui.QLabel('exposure_us')
+        file_index_label = QtGui.QLabel('file_index')
+        write_timestamp_label = QtGui.QLabel('write_timestamp')
+
+        acquisition_count_label = QtGui.QLabel('acquisition_count')
+        lens_status_label = QtGui.QLabel('lens_status')
+        gain_db_label = QtGui.QLabel('gain_db')
+        focal_length_mm_label = QtGui.QLabel('focal_length_mm')
+
+        row_offset_label = QtGui.QLabel('row_offset')
+        column_offset_label = QtGui.QLabel('column_offset')
+        num_rows_label = QtGui.QLabel('num_rows')
+        num_columns_label = QtGui.QLabel('num_columns')
+
+        scale_by_label = QtGui.QLabel('scale_by')
+        pixel_offset_label = QtGui.QLabel('pixel_offset')
+        pixel_scale_label = QtGui.QLabel('pixel_scale')
+        quality_label = QtGui.QLabel('quality')
+
+        layout.addWidget(frame_status_label, 0, 0)
+        layout.addWidget(frame_id_label, 1, 0)
+        layout.addWidget(frame_timestamp_ns, 2, 0)
+        layout.addWidget(focus_step_label, 3, 0)
+
+        layout.addWidget(aperture_stop_label, 0, 3)
+        layout.addWidget(exposure_us_label, 1, 3)
+        layout.addWidget(file_index_label, 2, 3)
+        layout.addWidget(write_timestamp_label, 3, 3)
+
+        layout.addWidget(acquisition_count_label, 0, 5)
+        layout.addWidget(lens_status_label, 1, 5)
+        layout.addWidget(gain_db_label, 2, 5)
+        layout.addWidget(focal_length_mm_label, 3, 5)
+
+        layout.addWidget(row_offset_label, 0, 7)
+        layout.addWidget(column_offset_label, 1, 7)
+        layout.addWidget(num_rows_label, 2, 7)
+        layout.addWidget(num_columns_label, 3, 7)
+
+        layout.addWidget(scale_by_label, 0, 5)
+        layout.addWidget(pixel_offset_label, 1, 5)
+        layout.addWidget(pixel_scale_label, 2, 5)
+        layout.addWidget(quality_label, 3, 5)
+
+        nested_widget.setLayout(layout)
+        self.setWidget(nested_widget)
+
 
 if __name__ == "__main__":
     from pmc_turbo.utils import log
     import sys
+
     camera_id = None
-    if len(sys.argv)>1:
+    if len(sys.argv) > 1:
         camera_id = int(sys.argv[1])
     log.setup_stream_handler(log.logging.DEBUG)
     app = QtGui.QApplication([])
     dw = QtGui.QDesktopWidget()
+    iw = InfoBar()
     win = QtGui.QMainWindow()
-    win.resize(800,800)
+    win.resize(800, 800)
     imv = MyImageView(camera_id)
     win.setCentralWidget(imv)
+    win.addDockWidget(QtCore.Qt.BottomDockWidgetArea, iw)
     win.show()
     if camera_id is not None:
         if dw.screenCount() > 1:
@@ -81,17 +149,15 @@ if __name__ == "__main__":
             geom = dw.availableGeometry()
             h = geom.height()
             w = geom.width()
-            h0 = h/2.*(camera_id%2) + geom.top()
-            w0 = w/4.0*(camera_id//2) + geom.left()
-            win.setMaximumHeight(h/2.0)
-            win.setMaximumWidth(w/4.0)
-            win.move(h0,w0)
+            h0 = h / 2. * (camera_id % 2) + geom.top()
+            w0 = w / 4.0 * (camera_id // 2) + geom.left()
+            win.setMaximumHeight(h / 2.0)
+            win.setMaximumWidth(w / 4.0)
+            win.move(h0, w0)
 
-    #timer = QtCore.QTimer()
-    #timer.timeout.connect(imv.update)
+    # timer = QtCore.QTimer()
+    # timer.timeout.connect(imv.update)
     ##timer.start(1000)
 
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
-
-
