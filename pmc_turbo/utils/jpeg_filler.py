ae144e748c8631de78e299b8f070fbc01e8f632b
ckjellstrand <bkjell@gmail.com>
2017-07-25 19:03:36 -0400

    jpeg_filler complete - for filling in missing packets in jpeg_files with zeros.

--- pmc_turbo/utils/jpeg_filler.py
+++ pmc_turbo/utils/jpeg_filler.py
@@ -1,23 +1,27 @@
 from pmc_turbo.communication import packet_classes, file_format_classes
 import numpy as np
+import sys
 
 
 def construct_jpeg(packets):
     sorted_packets = sorted(packets, key=lambda k: k.packet_number)
     packet_length = sorted_packets[0].payload_length
     data_buffer = ''
-    i = 0
-    while i < len(sorted_packets):
-        packet_number = sorted_packets[i].packet_number
-        if packet_number == i:
-            data_buffer += sorted_packets[i].payload
-            i += 1
-        if packet_number > i:
-            data_buffer += np.zeros(packet_length, dtype=np.uint8)
-            i += 1
+    packet_number = 0
+
+    for packet in sorted_packets:
+        if packet_number == packet.packet_number:
+            data_buffer += packet.payload
+            packet_number += 1
         else:
-            data_buffer += np.zeros((packet_length - i), dtype=np.uint8)
-            i = packet_number
+            if packet_number < packet.packet_number:
+                missing_packets = packet.packet_number - packet_number
+                data_buffer += np.zeros(missing_packets * packet_length).astype('uint8').tostring()
+                data_buffer += packet.payload
+                packet_number = packet.packet_number + 1
+            else:
+                raise Exception('intended packet_number greater than actual packet_number')
+
     return data_buffer
 
 
@@ -37,7 +41,19 @@ def put_data_into_packets(buffer, file_id, packet_size=1000):
     return packets
 
 
-def _get_data_for_tests():
+def get_data_for_example():
     with open('2017-07-25_174523_002255.jpg', 'rb') as f:
         buffer = f.read()
     return buffer
+
+
+def run_example():
+    buffer = get_data_for_example()
+    packets = put_data_into_packets(buffer, 1)
+    partial_packets = packets[:3] + packets[5:]
+    j2 = construct_jpeg(partial_packets)
+    with open('/home/bjorn/jpeg_filler_example.jpg', 'wb') as f:
+        f.write(j2)
+
+if __name__ == "__main__":
+    run_example()
